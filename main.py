0189e38c3ebfaaefdf2bf1a90926a0699709f345
cnthcr <157423671+cnthcr@users.noreply.github.com>
2024-11-14 21:10:50 -0300

    Criando Menu principal

---
+++ main.py
@@ -0,0 +1,24 @@
+def main():
+    while True:
+        print("\nMenu Principal")
+        print("1. Vendas")
+        print("2. Produtos")
+        print("3. Fornecedores")
+        print("4. Sair")
+        
+        choice = input("Escolha uma opção: ")
+        
+        if choice == '1':
+            pass
+        elif choice == '2':
+            pass
+        elif choice == '3':
+            pass
+        elif choice == '4':
+            print("Saindo do sistema.")
+            break
+        else:
+            print("Opção inválida.")
+
+if __name__ == "__main__":
+    main()
